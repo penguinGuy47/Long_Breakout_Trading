8f9c1f428382ac439d9e6f9aefe703ed665af24d
Kaleb <100792840+penguinGuy47@users.noreply.github.com>
2025-10-03 09:43:12 -0500

    Delete supreme_bot directory

--- supreme_bot/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd C:\Users\%USERNAME%\Desktop\Code\Python\supreme_bot || (echo "Failed to navigate to the project directory" & pause & exit) 
-call .\myenv\Scripts\activate || (echo "Failed to activate virtual environment" & pause & exit) 
-python main.py 
-pause
